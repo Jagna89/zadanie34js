526406bfffe0ff74e82b1c0237e74b5f070c5467
Your Name <you@example.com>
2021-12-14 16:37:59 +0100

    js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let text='Uwielbiam JavaScript';
+let komunikat='Jestem świetnym programistą';
+
+console.log(text)
+console.log(komunikat)
+
+function add(text, komunikat)
+{
+  let result=text + komunikat;
+return result;
+
+}
+
+add (text, komunikat);
